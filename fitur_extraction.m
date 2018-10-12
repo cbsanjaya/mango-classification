8bc40a1730e6e7c4b9a276fce9ae5ff64d069c10
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-10-12 07:42:38 +0700

    fungsi fitur extraction

---
+++ fitur_extraction.m
@@ -0,0 +1,67 @@
+function [metric,eccentricity,Contrast,Correlation,Energy,Homogeneity, class_label] = fitur_extraction(full_name)
+%start computation time
+tic
+
+[name] = fileparts(full_name);
+isMature = name(4:6);
+
+if strcmp(isMature, 'mtr')
+    class_label = 1;
+else
+    class_label = 0;
+end
+
+Img = imread(full_name);
+
+% Color-Based Segmentation Using K-Means Clustering
+cform = makecform('srgb2lab');
+lab = applycform(Img,cform);
+
+ab = double(lab(:,:,2:3));
+nrows = size(ab,1);
+ncols = size(ab,2);
+ab = reshape(ab,nrows*ncols,2);
+
+nColors = 2;
+[cluster_idx, ~] = kmeans(ab,nColors,'distance','sqEuclidean', ...
+    'Replicates',3);
+
+pixel_labels = reshape(cluster_idx,nrows,ncols);
+
+segmented_images = cell(1,3);
+rgb_label = repmat(pixel_labels,[1 1 3]);
+
+for k = 1:nColors
+    color = Img;
+    color(rgb_label ~= k) = 0;
+    segmented_images{k} = color;
+end
+
+area_cluster1 = sum(find(pixel_labels==1));
+area_cluster2 = sum(find(pixel_labels==2));
+
+[~,cluster_min] = min([area_cluster1,area_cluster2]);
+
+Img_bw = (pixel_labels==cluster_min);
+Img_bw = imfill(Img_bw,'holes');
+Img_bw = bwareaopen(Img_bw,50);
+
+stats = regionprops(Img_bw,'Area','Perimeter','Eccentricity');
+area = stats.Area;
+perimeter = stats.Perimeter;
+metric = 4*pi*area/(perimeter^2);
+eccentricity = stats.Eccentricity;
+
+Img_gray = rgb2gray(Img);
+Img_gray(~Img_bw) = 0;
+
+pixel_dist = 1;
+GLCM = graycomatrix(Img_gray,'Offset',[0 pixel_dist; -pixel_dist pixel_dist; -pixel_dist 0; -pixel_dist -pixel_dist]);
+stats = graycoprops(GLCM,{'contrast','correlation','energy','homogeneity'});
+Contrast = mean(stats.Contrast);
+Correlation = mean(stats.Correlation);
+Energy = mean(stats.Energy);
+Homogeneity = mean(stats.Homogeneity);
+
+% end computation time
+toc
